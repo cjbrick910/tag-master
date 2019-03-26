d82335effabdde584f29b603bdca3c303820ab49
Chris House <christopher@littlehouseofdesign.com>
2019-03-26 12:27:50 -0400

    added wpf app

---
+++ tagmast/tagmast/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tagmast
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static void Main(string[] args)
+        {
+            List<string> arrHeaders = new List<string>();
+
+            Shell32.Shell shell = new Shell32.Shell();
+            Shell32.Folder objFolder;
+
+            objFolder = shell.NameSpace(@"C:\temp\testprop");
+
+            for (int i = 0; i < short.MaxValue; i++)
+            {
+                string header = objFolder.GetDetailsOf(null, i);
+                if (String.IsNullOrEmpty(header))
+                    break;
+                arrHeaders.Add(header);
+            }
+
+            foreach (Shell32.FolderItem2 item in objFolder.Items())
+            {
+                for (int i = 0; i < arrHeaders.Count; i++)
+                {
+                    Console.WriteLine(
+                      $"{i}\t{arrHeaders[i]}: {objFolder.GetDetailsOf(item, i)}");
+                }
+            }
+        }
+    }
+}
