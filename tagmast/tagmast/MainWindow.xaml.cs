1227fe29bf03653e8b57e347c3bb75cb473d3b22
Chris House <christopher@littlehouseofdesign.com>
2019-03-28 12:42:19 -0400

    added comments

--- tagmast/tagmast/MainWindow.xaml.cs
+++ tagmast/tagmast/MainWindow.xaml.cs
@@ -32,47 +32,53 @@ namespace tagmast
         }
         public void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            // declares keyword variable as a string array with 1 element inside
             string[] keywords = new string[1];
+            //sends user input to the keywords variable
             keywords[0] = textbox2.Text;
+            //grabs user iput of directory to a string
            string dir = TextBox1.Text;
+            //turns the string into a DirectoryInfo object di1
             DirectoryInfo di1 = new DirectoryInfo(dir);
+            //declares rootDir as another DirectoryInfo object with the same value as di1
             System.IO.DirectoryInfo rootDir = di1;
+            //sends the keywords variable and the di1 variable to the WalkDirectoryTree method, which does most of the processing
             WalkDirectoryTree(di1, keywords);
 
         }
         public static void WalkDirectoryTree(System.IO.DirectoryInfo root, string[] keyword)
         {
+            //creates fileinfo and directoryinfo objects for use later
             System.IO.FileInfo[] files = null;
             System.IO.DirectoryInfo[] subDirs = null;
 
-            // First, process all the files directly under this folder
+            // gets all files and subdirectories inside the rootDir
             try
             {
                 files = root.GetFiles("*.*");
             }
-
+            //writes a console message for if a directory is not found
             catch (System.IO.DirectoryNotFoundException e)
             {
                 Console.WriteLine(e.Message);
             }
-
+            //main processing loop
             if (files != null)
             {
+                
                 foreach (System.IO.FileInfo fi in files)
                 {
-                    // In this example, we only access the existing FileInfo object. If we
-                    // want to open, delete or modify the file, then
-                    // a try-catch block is required here to handle the case
-                    // where the file has been deleted since the call to TraverseTree().
+                   //for each loop, the program grabs the full path of each file in the directory, and puts it into a string <file>
                     try
                     {
                     string file = fi.FullName;
+                    //uses the path to create a new ShellFile object for adding complex file properties
                     var shellFile = ShellFile.FromFilePath(file);
+                    //sets the keyword property of the ShellFile object to the value in <keyword>
                     shellFile.Properties.System.Keywords.Value = keyword;
                     }
-       
-                    catch (Microsoft.WindowsAPICodePack.Shell.PropertySystem.PropertySystemException e)
+                    //if the file is not a media file, program throws a message box with the file name and an OK button
+                    catch (Microsoft.WindowsAPICodePack.Shell.PropertySystem.PropertySystemException)
                     {
                         string caption = "Error: non media files found";
                         string message = fi.FullName;
